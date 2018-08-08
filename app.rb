3dfac1e86a70cf4bc3e9aa2c1a33b0a2cffc2498
Jamie Brown <jmbrowncow@gmail.com>
2018-08-08 15:23:39 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -11,18 +11,16 @@ class App < Sinatra::Base
   end
 
   get '/say/:number/:phrase' do
-    # @number = params[:number].to_i
-    # @number.times do
-    #   "#{params[:phrase]}"
-    # end
-
     number = params[:number].to_i
     phrase = "#{params[:phrase]}"
     new_phrase = ""
     number.times do
-      new_phrase = new_phrase + " " + phrase 
+      new_phrase = new_phrase + " " + phrase
     end
     new_phrase
   end
 
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}"
+  end
 end
